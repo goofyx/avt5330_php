56e5d078b133c32d1e2531c115610823d5b61e08
goofyx <k.faryna@goofyx.pl>
2017-10-04 10:43:44 +0000

    git #1 PHP: odczyt temperatury z karty

---
+++ temp3.php
@@ -0,0 +1,18 @@
+<?php
+include "PHPcom/PHPcom.php";
+
+$com = new PHPcom( "/dev/ttyUSB0", 19200, 8, 1 );
+$com->Setup( );
+
+$com->Open( );
+
+while(1){
+	$temp = $com->read(200,"\n");
+	if ($temp <> ""){
+		echo( $temp."\n" );
+	}
+}
+
+
+$com->Close( );
+?>
